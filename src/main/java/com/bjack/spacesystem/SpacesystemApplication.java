74abc1a8b7c2f448367246310ff691932e33d6ea
JackyFlip <bjack2218@gmail.com>
2022-05-24 10:59:38 +0200

    First commit

---
+++ src/main/java/com/bjack/spacesystem/SpacesystemApplication.java
@@ -0,0 +1,13 @@
+package com.bjack.spacesystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpacesystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpacesystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bjack/spacesystem/SpacesystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bjack.spacesystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpacesystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
